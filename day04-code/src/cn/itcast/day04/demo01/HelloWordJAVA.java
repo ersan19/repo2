2c8a06a52d4043d6d500ea519bf3a5913892f82a
ersan <1426970194@qq.com>
2020-06-21 16:34:41 +0800

    第一次idea

---
+++ day04-code/src/cn/itcast/day04/demo01/HelloWordJAVA.java
@@ -0,0 +1,11 @@
+package cn.itcast.day04.demo01;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HelloWordJAVA {
+    public static void main(String[] args) {
+        System.out.println("wwo ai java");
+    }
+    }
+
+
