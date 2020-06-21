ab9ba61f0c09f00a70bc47bd5407cc71de0f03e1
ersan <1426970194@qq.com>
2020-06-21 16:50:59 +0800

    修改

--- day04-code/src/cn/itcast/day04/demo01/HelloWordJAVA.java
+++ day04-code/src/cn/itcast/day04/demo01/HelloWordJAVA.java
@@ -5,7 +5,11 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class HelloWordJAVA {
     public static void main(String[] args) {
         System.out.println("wwo ai java");
-    }
+        tese();
     }
 
+    public static void tese(){
+        System.out.println(11);
+    }
 
+}
